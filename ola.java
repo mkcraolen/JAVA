eb6d5ab38e5754cad0aaa9114d57f3eeea849751
Maikon Araujo <68927435+mkcraolen@users.noreply.github.com>
2020-08-06 20:32:32 -0300

    Delete ola.java

--- ola.java
+++
@@ -1,8 +0,0 @@
-/**
- * Exemplo de uma classe simples em Java.
- */
-public class PrimeiraClasse {
-    public static void main(String[] args) {
-        System.out.println("Ola Arbyte !!!");
-    }
-}
