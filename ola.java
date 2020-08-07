c45cd400bafb9129f09edae13f93cfae2ef63c0a
Maikon Araujo <maikonaraujo2@outlook.com>
2020-08-06 21:20:10 -0300

    ola.java

---
+++ ola.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+ 
+  public static void main(String[] args) {
+      HelloWorld hello = new HelloWorld();
+      hello.imprimeTexto("Alô mundo!");
+  }
+ 
+  public void imprimeTexto(String texto){
+      System.out.println(texto);
+  }
+ 
+}
